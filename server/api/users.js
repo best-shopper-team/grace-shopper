1731b1fbc6c1c7203db004367653b0d634a344c4
Alyssa Venere Braun <alyssavenere@gmail.com>
2018-01-09 16:14:38 -0600

    users api routes updated, reviews api routes created

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+// return all users
 router.get('/', (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
@@ -10,5 +11,16 @@ router.get('/', (req, res, next) => {
     attributes: ['id', 'email']
   })
     .then(users => res.json(users))
+    .catch(next)
+})
+
+// updates one user
+router.put('/:userId', (req, res, next) => {
+  User.findOne({
+    where: {
+      id: req.params.userId
+    }
+  })
+    .then(foundUser => foundUser.update(req.body))
     .catch(next)
 })
