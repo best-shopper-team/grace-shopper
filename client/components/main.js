a9385430cc44a607f6f20126f64a01b0873bfbf5
Alec Davidson <davidson9076@gmail.com>
2018-01-10 15:13:08 -0600

    add links to products page in navbar

--- client/components/main.js
+++ client/components/main.js
@@ -23,11 +23,13 @@ const Main = (props) => {
               {/* The navbar will show these links after you log in */}
               <Link to="/home">Home</Link>
               <a href="#" onClick={handleClick}>Logout</a>
+              <Link to="/products">Products</Link>
             </div>
             : <div>
               {/* The navbar will show these links before you log in */}
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
+              <Link to="/products">Products</Link>
             </div>
         }
       </nav>
