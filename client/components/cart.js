c1e3a3662244c0726e20690327e68f7d32c18e91
jt wang <jtjtjtjtw@gmail.com>
2018-01-11 11:54:25 -0600

    cart page has subtotals

--- client/components/cart.js
+++ client/components/cart.js
@@ -9,32 +9,57 @@ import {getCartFromDb, me} from '../store'
 export class Cart extends Component{
   constructor(props){
     super(props)
+    this.proceedCheckout = this.proceedCheckout.bind(this)
   }
 
   componentDidMount(){
     this.props.getCart(this.props.user.id)
-    // if(!this.props.user.id){
+  }
 
-    // }
+  proceedCheckout(e){
+    console.log('event', e)
   }
 
   render(){
     const cart = this.props.cart
+    const products = this.props.allProducts
     return (
       <div className="cartContainer">
         <h3>This is your cart</h3>
         { cart.orderitems && cart.orderitems.map((orderItem) => {
           return (
             <div key={orderItem.id} className="orderItem">
-            <h4>This is an orderitem {orderItem.id}</h4>
+            <h4>{
+              (products.find((prod) => prod.id === orderItem.productId)).title
+            }</h4>
               <div className="item-info">
-                <span>Price {orderItem.itemPrice}</span>
-                <span>Quantity {orderItem.quantity}</span>
-                <span>Item Total {orderItem.itemTotal}</span>
+                <span>ProductId:
+                  {orderItem.id}
+                </span>
+                <span>Price:
+                  {parseFloat(orderItem.itemPrice * 0.01).toFixed(2)}
+                </span>
+                <span>Quantity:
+                  {orderItem.quantity}
+                </span>
+                <span>Item Total:
+                  {parseFloat(orderItem.itemTotal * 0.01).toFixed(2)}
+                </span>
               </div>
             </div>
           )
         })}
+        <h4>Subtotal: {
+          cart.orderitems &&
+            parseFloat(
+              (cart.orderitems.map(orderitem => orderitem.itemTotal).reduce((accum, currentVal) => accum + currentVal))
+              * 0.01)
+              .toFixed(2)
+        }
+        </h4>
+        <button onClick={e => this.proceedCheckout(e)} className="ui button">
+        Proceed to Checkout
+        </button>
       </div>
     )
   }
@@ -46,7 +71,8 @@ export class Cart extends Component{
 const mapState = (state) => {
   return {
     cart: state.cart,
-    user: state.user
+    user: state.user,
+    allProducts: state.allProducts
   }
 }
 
