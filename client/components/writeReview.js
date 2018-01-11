da8104b9b0b2b050b53f1669d514320169207da6
lcbuchanan <buchanan.lori@gmail.com>
2018-01-10 20:50:23 -0600

    completes funcitonality of writeReview component, posts to database

--- client/components/writeReview.js
+++ client/components/writeReview.js
@@ -1,45 +1,91 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { fetchProduct } from '../store'
+import { fetchProduct, postProductReview, me } from '../store'
 
 
-class WriteReview extends Component{
+class WriteReview extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      rating: 0,
+      content: '',
+      fireRedirect: false
+    }
+    this.handleOptionChange = this.handleOptionChange.bind(this)
+    this.inputDescription = this.inputDescription.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
   componentDidMount(){
     this.props.getProduct();
   }
 
+  handleOptionChange(evt){
+    this.setState({
+      rating: evt.target.value
+    })
+  }
+
+  inputDescription(evt){
+    console.log('you typed: ', evt.target.value)
+    this.setState({
+      content: evt.target.value
+    })
+  }
+
+  handleSubmit(evt){
+    evt.preventDefault();
+    const review = {
+      rating: this.state.rating,
+      content: this.state.content,
+      userId: this.props.user.id,
+      productId: this.props.product.id
+    }
+    this.props.addReview(review)
+    this.setState({fireRedirect: true})
+  }
+
   render(){
     const { product } = this.props
 
-    console.log("product: ", product)
 
-    return(
+    return (
       <div>
-        <h3>Review {product.title}</h3>
-        <form>
-          <div id="rating-radio">
-            Rating:
-            <input type="radio" name="rating" value="1"></input>
-            <div>1</div>
-            <input type="radio" name="rating" value="2"></input>
-            <div>2</div>
-            <input type="radio" name="rating" value="3"></input>
-            <div>3</div>
-            <input type="radio" name="rating" value="4"></input>
-            <div>4</div>
-            <input type="radio" name="rating" value="5"></input>
-            <div>5</div>
-          </div>
-          <div id="review-input">
-            Review:
-            <textarea type="text"></textarea>
-          </div>
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
+      {this.state.fireRedirect ? <h1>Thanks for your review!</h1> :
+        <div>
+          <h3>Review {product.title}</h3>
+          <form onSubmit={this.handleSubmit}>
+            <div id="rating-radio">
+              Rating:
+              <input type="radio" name="rating" value="1"
+              onChange={this.handleOptionChange} />
+              <div>1</div>
+              <input type="radio" name="rating" value="2"
+              onChange={this.handleOptionChange} />
+              <div>2</div>
+              <input type="radio" name="rating" value="3"
+              onChange={this.handleOptionChange} />
+              <div>3</div>
+              <input type="radio" name="rating" value="4"
+              onChange={this.handleOptionChange}/>
+              <div>4</div>
+              <input type="radio" name="rating" value="5"
+              onChange={this.handleOptionChange}/>
+              <div>5</div>
+            </div>
+            <div id="review-input">
+              Review:
+              <textarea type="text" value={this.state.content}
+              name="content" onChange={this.inputDescription} />
+            </div>
+            <div>
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      }
       </div>
     )
   }
@@ -57,6 +103,12 @@ const mapDispatch = (dispatch, ownProps) => {
   return {
     getProduct () {
       dispatch(fetchProduct(productId))
+    },
+    getCurrentUser () {
+      dispatch(me())
+    },
+    addReview (review) {
+      dispatch(postProductReview(review))
     }
   }
 }
