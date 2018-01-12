d825b7627175075230cff5b165587f2bd25eb8d1
Alyssa Venere Braun <alyssavenere@gmail.com>
2018-01-12 16:06:09 -0600

    added admin banner

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -4,11 +4,18 @@ import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 import {fetchProduct, fetchProductReviews} from '../store'
 import history from '../history'
+import {Message, Button, Container, Rating, Grid, Image, Dropdown} from 'semantic-ui-react'
 
 export class SingleProduct extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.state = {
+      id: props.match.params.productId,
+      quantity: null
+    }
     this.adminEditClick = this.adminEditClick.bind(this)
+    this.addToCart = this.addToCart.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount(){
@@ -20,6 +27,15 @@ export class SingleProduct extends React.Component {
     history.push(`/admin/products/${this.props.product.id}/edit`)
   }
 
+  addToCart(event){
+    event.preventDefault();
+  }
+
+  handleChange(event){
+    this.setState({ [event.target.name]: event.target.value });
+    console.log('this.state: ', this.state)
+  }
+
   render(){
     let { product, ratingArray, user } = this.props;
 
@@ -27,18 +43,56 @@ export class SingleProduct extends React.Component {
     let dollarPrice = product.price / 100;
 
     return (
-      <div>
-        <h3>{product.title}</h3>
+      <Container>
+        <h2>{product.title}</h2>
         {
-          user.isAdmin && <button onClick={this.adminEditClick}>Edit Product</button>
+          user.isAdmin &&
+          <div>
+            {
+              product.isAvailable ?
+              <Message positive>
+                <Message.Header>This product is ACTIVE.</Message.Header>
+                <br />
+                <Button classic onClick={this.adminEditClick}>Edit Product</Button>
+              </Message>
+              : <Message negative>
+                <Message.Header>This product is INACTIVE.</Message.Header>
+                <br />
+                <button onClick={this.adminEditClick}>Edit Product</button>
+              </Message>
+            }
+          </div>
         }
-        <br />
-        ${dollarPrice}<br />
-        Average star rating: {avgRating}/5<br />
-        <img id="single-product-page-image" src={`${product.photoUrl}`} /><br />
-        Only {product.quantity} remaining!<br />
-        {product.description}<br />
-      </div>
+        <Grid>
+          <Grid.Column width={7}>
+            <Image src={`${product.photoUrl}`} />
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <br />
+            {
+              dollarPrice % 2 ?
+              <div>${dollarPrice}0</div>
+              : <div>${dollarPrice}</div>
+            }
+            <br />
+            Average rating: <Rating defaultRating={avgRating} maxRating={5} disabled />
+            <br />
+            <br />
+            <p>{product.description}</p>
+            <p>ONLY {product.quantity} REMAINING!</p>
+            <br />
+            <br />
+            <input type="text" name="quantity" defaultValue="0" onChange={this.handleChange} />
+            <Button
+              content="Add to Cart"
+              icon="shop"
+              labelPosition="left"
+              onClick={this.addToCart}
+            />
+          </Grid.Column>
+        </Grid>
+
+      </Container>
     )
   }
 }
@@ -50,7 +104,8 @@ const mapState = (state) => {
   return {
     user: state.user,
     product: state.singleProduct,
-    ratingArray: state.reviews.map(review => review.rating)
+    ratingArray: state.reviews.map(review => review.rating),
+    ratingLength: state.reviews.length
   }
 }
 
