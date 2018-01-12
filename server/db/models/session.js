9b7b7498d69e3f11f753dac2ec29e78edffc7d16
lcbuchanan <buchanan.lori@gmail.com>
2018-01-12 09:56:02 -0600

    remove session model and validates email on user model

--- server/db/models/session.js
+++ server/db/models/session.js
@@ -1,14 +1,1 @@
 
-const Sequelize = require('sequelize')
-const db = require('../db')
-
-const Session = db.define('Session', {
-  sid: {
-    type: Sequelize.INTEGER,
-    primaryKey: true
-  },
-  expires: Sequelize.DATE,
-  data: Sequelize.JSON
-})
-
-module.exports = Session;
