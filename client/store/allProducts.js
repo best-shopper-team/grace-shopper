69858b8b8450b8b312d874dcf7d19e2178c5cb8c
jintobeans <wang.jinting01@gmail.com>
2018-01-15 12:09:24 -0600

    Update allProducts.js

--- client/store/allProducts.js
+++ client/store/allProducts.js
@@ -47,7 +47,6 @@ export function postProduct(product) {
 
 export const editProduct = product =>
   dispatch => {
-    console.log('product: ', product)
     return axios.put(`/api/products/${product.id}`, product)
       .then(res => {
         return dispatch(updateProduct(res.data))
